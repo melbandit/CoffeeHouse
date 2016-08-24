d316910f77952002f7864af094bab7515ad91489
melbandit <melissa.i.sattler@gmail.com>
2016-08-23 23:21:00 -0400

    asdf

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,31 @@
-$('.carousel').carousel({
-  interval: 4000
-})
+// $('.carousel').carousel({
+//   interval: 4000
+// });
+
+$(".prep").click(function(){
+	$('#prep').addClass("active");
+	$('.prep').addClass("active");
+	$('#source').removeClass("active");
+	$('.source').removeClass("active");
+	$('#features').removeClass("active");
+	$('.features').removeClass("active");
+});
+
+$(".features").click(function(){
+	$('#prep').removeClass("active");
+	$('.prep').removeClass("active");
+	$('#source').removeClass("active");
+	$('.source').removeClass("active");
+	$('#features').addClass("active");
+	$('.features').addClass("active");
+});
+
+$(".source").click(function(){
+	$('#prep').removeClass("active");
+	$('.prep').removeClass("active");
+	$('#source').addClass("active");
+	$('.source').addClass("active");
+	$('#features').removeClass("active");
+	$('.features').removeClass("active");
+});
+
